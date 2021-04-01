0a4a71c376583d60e0a4d653111826f418a772e4
root <root@pete.pete>
2021-03-31 21:31:42 -0400

    Added css and better login handling

--- html/login.php
+++ html/login.php
@@ -1,37 +1,61 @@
 <?php include "./inc/dbinfo.inc"; ?>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>Register Form</title>
-</head>
 
-<body>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-6">
+    <meta name="viewport" content="width=device-width, initial-scale=2">
+    <title>Scandinavian Defense Login</title>
+
+    <!-- Define styles -->
+    <link rel="stylesheet" href="main.css">
+  </head>
+
+  <body>
+
+    <!-- Login Form -->
+    <div class="login-page">
+      <div class="form">
+        <div class="login">
+          <div class="login-header">
+            <h3>Login</h3>
+          </div>
+        </div>
+        <form name="loginForm" method="post" action="login.php">
+            <input type="text" id="userid" name="userid" placeholder="Username" required>
+            <input type="Password" id="pwd" name="pwd" placeholder="Password" required>
+            <input type="Reset">
+            <input type="submit" value="Login"\>
+            <p class="message">Not registered?
+              <a href="register.html">Create an account</a></p>
+        </form>
+      </div>
+    </div>
+
 <?php
-    $username = $_POST["userid"];
-    $pwd = $_POST["pwd"];
+  $username = $_POST["userid"];
+  $pwd = $_POST["pwd"];
 
-    /* Connect to database thru ./inc/dbinfo.inc file */
-    $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
-    if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    $database = mysqli_select_db($connection, DB_DATABASE);
-    $table = "users";
+  /* Connect to database thru ./inc/dbinfo.inc file */
+  $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+  if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  $database = mysqli_select_db($connection, DB_DATABASE);
+  $table = "users";
 
-    /* Accept new session if user/password is valid, otherwise reject */
-    /* The BINARY keyword makes sure its case-sensitive. Note it's used for pwd but not username */
-    $result = mysqli_query($connection,
-        "SELECT user_id, pwd FROM $table WHERE user_id='$username' AND pwd=BINARY '$pwd'");
+  /* Accept new session if user/password is valid, otherwise reject */
+  /* The BINARY keyword makes sure it's case-sensitive. Note it's used for pwd but not username */
+  $result = mysqli_query($connection,
+    "SELECT user_id, pwd FROM $table WHERE user_id='$username' AND pwd=BINARY '$pwd'");
 
-    if(mysqli_num_rows($result) > 0){
-        $_SESSION["logged_in"] = true;
-        $_SESSION["name"] = $username;
-        echo "Welcome, $username";
-    }
-    else{
-        echo "Username or password is incorrect";
-        return false;
-    }
+  if(mysqli_num_rows($result) > 0){
+    $_SESSION["logged_in"] = true;
+    $_SESSION["name"] = $username;
+    header("Location:hello.php");
+  }
+  else{
+    echo '<script>alert("Username or password is incorrect")</script>';
+  }
 
 ?>
 </body>
-
 </html>
--- html/register.php
+++ html/register.php
@@ -3,45 +3,66 @@
 <head>
 <meta charset="UTF-8">
 <title>Register Form</title>
+<link rel="stylesheet" href="main.css">
 </head>
 
 <body>
+
+  <!-- Register Form -->
+  <div class="register-page">
+    <div class="form">
+      <div class="register">
+        <div class="register-header">
+          <h3>Register</h3>
+        </div>
+      </div>
+      <form id="registerForm" action="register.php" method="post">
+        <input type="text" id="username" name="username" placeholder="Username" required/>
+        <input type="password" id="psw" name="psw" placeholder="Password" required/>
+        <input type="password" id="psw-repeat" name="psw-repeat" placeholder="Repeat Password" required/>
+        <button>Register</button>
+      </form>
+    </div>
+  </div>
+
+
 <?php
-    /* Check passwords match */
-    if ($_POST["psw"] != $_POST["psw-repeat"]){
-        echo "Passwords do not match";
-        return false;
-    }
 
-    /* Connect to database thru ./inc/dbinfo.inc file */
-    $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
-    if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    $database = mysqli_select_db($connection, DB_DATABASE);
-    $table = "users";
+  /* Check passwords match */
+  if ($_POST["psw"] != $_POST["psw-repeat"]){
+    echo '<script>alert("Passwords do not match")</script>';
+    return false;
+  }
+
+  /* Connect to database thru ./inc/dbinfo.inc file */
+  $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+  if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  $database = mysqli_select_db($connection, DB_DATABASE);
+  $table = "users";
 
 
-    /* Return if username exists */
-    $username = $_POST["username"];
-    $psw = $_POST["psw"];
-    
-    $sql_u = mysqli_query($connection, 
-        "SELECT * FROM $table WHERE user_id='$username'");
-    if (mysqli_num_rows($sql_u) > 0){
-        echo 'Username has been taken';
-        return false;
-    }
+  /* Return if username exists */
+  $username = $_POST["username"];
+  $psw = $_POST["psw"];
+  
+  $sql_u = mysqli_query($connection, 
+    "SELECT * FROM $table WHERE user_id='$username'");
+  if (mysqli_num_rows($sql_u) > 0){
+    echo '<script>alert("Username has been taken")</script>';
+    return false;
+  }
 
-    /* Add new user into database */
-    $result = mysqli_query($connection,
-        "INSERT $table SET user_id='$username', pwd='$psw'");
-    if ($result){
-        echo "Registration successful!";
-        return true;
-    }
-    else{
-        echo "Registration failed";
-        return false;
-    }
+  /* Add new user into database */
+  $result = mysqli_query($connection,
+    "INSERT $table SET user_id='$username', pwd='$psw'");
+  if ($result){
+    echo '<script>alert("Registration successful!")</script>';
+    return true;
+  }
+  else{
+    echo "Registration failed";
+    return false;
+  }
 ?>
 </body>
 
